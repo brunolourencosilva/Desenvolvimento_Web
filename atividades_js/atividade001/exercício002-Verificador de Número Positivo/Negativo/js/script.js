37111ff41d7f7bbdea523503c28bafd69b46ed68
brunolourencosilva <brunolourenco.silva@gmail.com>
2025-05-08 21:39:36 -0300

    fazer css,js feito possivelmente total funcional

--- atividades_js/atividade001/exercício002-Verificador de Número Positivo/Negativo/js/script.js
+++ atividades_js/atividade001/exercício002-Verificador de Número Positivo/Negativo/js/script.js
@@ -1,52 +1,20 @@
-// Aguarda o carregamento completo do HTML antes de rodar o script
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function(){
 
-    // Obtém a referência ao elemento <ul> com id "ListaBreakContinue"
-    const ul = document.getElementById('listaBreakContinue');
+    const resultado = document.getElementById("resultado");
 
-    // Função que limpa o conteúdo da lista (Zera o conteudo da <ul>)
-    const limpar = () => ul.innerHTML = '';
 
-    // Função que adiciona um item <li> com texto especificado dentro da <ul>
-    const add = texto => {
-        // Cria um novo elemento <li>
-        const li = document.createElement('li');
-        // Define o texto do <li>
-        li.textContent = texto;
-        // adiciona o <li> como filho da <ul>
-        //append: adiciona um elemento filho dentro de outro elemento HTML
-        ul.appendChild(li);
-    };
+    const verificar = document.getElementById("btnVerificar");
 
-    // Evento ao clicar no botão com id "btnBreak"
-    document.getElementById('btnBreak').addEventListener('click', function () {
-        limpar();
-        // loop de 1 a 50
-        for (let i = 1; i <= 50; i++) {
+    verificar.addEventListener("click", function () {
+        let numero_1 = Number(document.getElementById("numero_1").value);
 
-            // Se o número for divisivel por 13...
-            if (i % 13 === 0) {
-                // Adicona mensagem
-                add('Parou no ' + i + ' (divisivel por 13!)');
-                // Encerra o loop imediatamente
-                break;
-            }
-            // Caso não seja divisivel por 13, apenas adiciona o número na listagem
-            add(i);
+        if (numero_1 < 0) {
+            resultado.textContent = 'Resultado: Número Negativo!!';
+        } else if (numero_1 > 0) {
+            resultado.textContent = 'Resultado: Número Positivo!!';
+        } else {
+            resultado.textContent = 'Resultado: Número é igual a zero!!';
         }
     });
 
-    // Evento ao clicar no botão com id "btnContinue"
-    document.getElementById('btnContinue').addEventListener('click', function () {
-        limpar();
-        
-        // Loop 1 a 5
-        for (let i = 1;i <= 5; i++) {
-
-            // Se o número for 3, pula a iteração atual (não adiciona o 3)
-            if (i === 3) continue;
-            // Adiciona o número normalmente se não for 3
-            add(i);
-        }
-    });
 });
