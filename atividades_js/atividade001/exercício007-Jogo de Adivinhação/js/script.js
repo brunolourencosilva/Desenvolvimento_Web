1f732663b7becb9fc6d69d0ef09e52aa83c0668c
brunolourencosilva <brunolourenco.silva15@gmail.com>
2025-05-12 18:32:26 -0300

    exercicio007-COmpleto e funcional

--- atividades_js/atividade001/exercício007-Jogo de Adivinhação/js/script.js
+++ atividades_js/atividade001/exercício007-Jogo de Adivinhação/js/script.js
@@ -1,20 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Pegando os elementos da tela
   const input = document.getElementById("CampoEntrada");
   const btnVerificar = document.getElementById("btnVerificar");
-  const Resultado = document.getElementById("Resultado");
-  const NumeroSorteado = document.getElementById("NumeroSorteado");
+  const resultado = document.getElementById("Resultado");
+  const dica = document.getElementById("Dica");
+  const btnSortear = document.getElementById("btnSortear");
 
+  let numeroSorteado = 0;
+
+  // Quando clicar em "Re/Começar jogo"
+  btnSortear.addEventListener("click", function () {
+    numeroSorteado = Math.floor(Math.random() * 10) + 1;
+    dica.textContent = "Tente acerta o numero sorteado";
+    input.value = "";
+  });
+
+  // Quando clicar em "Verificar"
   btnVerificar.addEventListener("click", function () {
+    const palpite = Number(input.value);
 
-  const NumeroSorteado = document.getElementById("NumeroSorteado");
-
-  const numero = Math.floor(Math.random() * 10) + 1;
-
-  if (NumeroSorteado == 1){
-    
-  }
-  // Exibe no elemento
-  NumeroSorteado.textContent = `Número sorteado: ${numero}`;
-
+    if (palpite === numeroSorteado) {
+      dica.textContent = "Parabéns! Você acertou!";
+    } else if (palpite > numeroSorteado) {
+      dica.textContent = "Dica: Tente um número menor.";
+    } else if (palpite < numeroSorteado) {
+      dica.textContent = "Dica: Tente um número maior.";
+    }
   });
 });
+
+
