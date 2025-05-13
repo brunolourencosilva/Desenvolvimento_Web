f7ec2ae6e1a6ba3652e42a3759bc32f3097588ba
brunolourencosilva <brunolourenco.silva15@gmail.com>
2025-05-13 17:05:13 -0300

    codigo terminado com sucesso

--- atividades_js/atividade001/exercício009-Alternador de Tema/js/script.js
+++ atividades_js/atividade001/exercício009-Alternador de Tema/js/script.js
@@ -1,11 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const AlterCorBody= document.getElementById("AlterCorBody");
+  const AlterCorBody = document.getElementById("AlterCorBody");
+
   const btnMudarTema = document.getElementById("btnMudarTema");
   const container = document.getElementById("container");
 
-  btnMudarTema.addEventListener("click", function(){
-    AlterCorBody.style.backgroundColor = "#000000";
-    AlterCorBody.style.backgroundColor = "#000000";
-    container.style.backgroundColor = "#ffffff";
+  let temaEscuro = false;
+
+  btnMudarTema.addEventListener("click", function () {
+    if (!temaEscuro) {
+      // Tema escuro
+      AlterCorBody.style.backgroundColor = "#000000";
+
+      container.style.backgroundColor = "#ffffff";
+      container.style.color = "#000000";
+      temaEscuro = true;
+    } else {
+      // Tema claro
+      AlterCorBody.style.backgroundColor = "#ffffff";
+
+      container.style.backgroundColor = "#000000";
+      container.style.color = "#ffffff";
+      temaEscuro = false;
+    }
   });
 });
